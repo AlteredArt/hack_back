1c7321f6adbc1bcbd76f929cccf7cd7dd8dacd36
Altered Art <cherokeedreemer@gmail.com>
2020-02-04 10:11:32 -0700

    our first push

---
+++ db/migrate/20200204171009_create_days.rb
@@ -0,0 +1,11 @@
+class CreateDays < ActiveRecord::Migration[6.0]
+  def change
+    create_table :days do |t|
+      t.string :explanation
+      t.string :hdurl
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
